03ca481711c2a7d10d2eb11423597f8e50cccff7
stepan <scravetchii@ip-performance.co.uk>
2018-06-05 13:16:27 +0100

    users

--- api/views.py
+++ api/views.py
@@ -1,10 +1,15 @@
+from django.contrib.auth.models import User
 from django.http import JsonResponse
 from rest_framework import views
 from rest_framework.permissions import IsAuthenticated
+
+from api.serializers import UserSerializer
 
 
 class EchoView(views.APIView):
     permission_classes = (IsAuthenticated,)
 
     def get(self, request):
-        return JsonResponse({'test': 'test'})
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return JsonResponse({'users': serializer.data})
